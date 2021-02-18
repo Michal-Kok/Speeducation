a5fbf8985bbfc47a8ecca22d82a5db8e80dc3e65
Michal-Kok <pukkix17@gmail.com>
2021-02-18 23:28:35 +0100

    large desktops added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
 gsap.registerPlugin('ScrollTrigger');
-// idk what XD
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 // DOM
 const burger = document.querySelector('.nav__button--burger');
 const list = document.querySelector('.nav__list');
@@ -26,14 +22,14 @@ const openMenu = () => {
 }
 
 const showInput = () => {
-    gsap.to(searchButton, {y: -50, duration: .3, transitionDelay: 0, transition: 0});
+    gsap.to(searchButton, {y: -100, duration: .3, transitionDelay: 0, transition: 0});
     gsap.to(searchSubmitButton, {transform: 'translateY(0)', duration: .4, delay: .6, ease: "bounce.out"});
     searchingForm.classList.toggle('visible');
 }
 
 const closeInput = (e) => {
     e.preventDefault();
-    gsap.to(searchSubmitButton, {transform: 'translateY(-50px)', duration: .3});
+    gsap.to(searchSubmitButton, {transform: 'translateY(-100px)', duration: .3});
     gsap.to(searchButton, {y: 0, duration: .4, delay: .6, transitionDelay: 0, ease: "bounce.out"});
     searchingForm.classList.toggle('visible');
 }
